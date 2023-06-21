084dc57ba0e3c2633362ac517a6fa18b847bee79
Rico21x <105686982+Rico21x@users.noreply.github.com>
2023-06-21 08:32:17 -0500

    Update cashier.test.js

--- cashier.test.js
+++ cashier.test.js
@@ -5,12 +5,12 @@ describe('scanApples', () => {
     totalCost = 0; // Reset the totalCost before each test
   });
 
-  it('calculates the total cost correctly for one apple', () => {
+  it('calculates the total cost of one apple', () => {
     scanApples(1);
     expect(totalCost).toBe(0.50);
   });
 
-  it('calculates the total cost correctly for multiple apples', () => {
+  it('calculates the total cost of multiple apples', () => {
     scanApples(2);
     scanApples(3);
     expect(totalCost).toBe(2.50);
